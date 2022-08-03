7a4935c148b74fe8195268d88aa3d55e75884d68
Bruno <bpayet.gsm@free.fr>
2022-08-03 16:08:38 +0200

    [php bases e-commerce] suite

--- php-bases-e-commerce/details.php
+++ php-bases-e-commerce/details.php
@@ -5,15 +5,14 @@ include './data/products.php';
 
 $page = 'details';
 
+// session_destroy();
+
 if (isset($_GET['id']) && !empty($_GET['id'])) {
     $id = $_GET['id'];
     // get the data of the id
     foreach ($products as $product) {
         if ($id === $product['id']) {
             $currentProduct = $product;
-            // $name = $product['name'];
-            // $description = $product['description'];
-            // $price = $product['price'];
             break 1;
         }
     }
@@ -22,6 +21,36 @@ if (isset($_GET['id']) && !empty($_GET['id'])) {
 $pageTitle = "Détails: " . $currentProduct['name'];
 $customCssLinks = ['<link rel="stylesheet" href="/assets/details.css">'];
 
+
+//Controle de la quantite à ajouter au panier :
+
+$qteIsNeg = false;
+
+// echo "<pre>" . var_dump($_SESSION) . "</pre><br>";
+if ((isset($_POST['quantity']) && !empty($_POST['quantity']) && $_POST['quantity'] > 0)
+    || (isset($_POST['quantity']) && !empty($_POST['quantity']) && $_POST['quantity'] < 0
+        && isset($_SESSION['cart'][$currentProduct['id']]))
+) {
+    if (
+        $_POST['quantity'] < 0 &&
+        $_SESSION['cart'][$currentProduct['id']] < -$_POST['quantity']
+    ) {
+        $qteIsNeg = true;
+    } else {
+        if (isset($_SESSION['cart'][$currentProduct['id']])) {
+            //Si on supprime tous les articles, on enlève l'article du panier
+            if ($_SESSION['cart'][$currentProduct['id']] == -$_POST['quantity']) {
+                unset($_SESSION['cart'][$currentProduct['id']]);
+            } else {
+                // sinon on modifie la quantité dans le panier
+                $_SESSION['cart'][$currentProduct['id']] += $_POST['quantity'];
+            }
+        } else {
+            $_SESSION['cart'][$currentProduct['id']] = $_POST['quantity'];
+        }
+        header('location: ./cart.php');
+    }
+}
 
 ?>
 
@@ -43,10 +72,13 @@ $customCssLinks = ['<link rel="stylesheet" href="/assets/details.css">'];
             <div class="bottom-details">
                 <div class="price"><?= number_format($currentProduct['price'], 2) ?><span class="currency">€</span></div>
                 <form method="post" class="add-to-cart-form">
-                    <input type="number" name="quantity" placeholder="Quantité..">
+                    <input type="number" name="quantity" placeholder="Quantité...">
                     <input type="submit" value="Ajouter au panier">
                 </form>
             </div>
+            <?php if ($qteIsNeg) : ?>
+                <span style="color: red;text-align: right;">Vous ne pouvez pas retirer plus de produit qu'il n'y en a au panier !</span>
+            <?php endif ?>
         </div>
     </div>
 </main>
--- php-bases-e-commerce/partials/nav.php
+++ php-bases-e-commerce/partials/nav.php
@@ -9,17 +9,23 @@ if (isset($_SESSION['user']) && !empty($_SESSION['user']['email'])) {
     $connected = true;
     $toggleConnexion = "Déconnexion";
 }
+
+$nbPrdCart = 0;
+if (isset($_SESSION['cart'])) {
+    $nbPrdCart = count($_SESSION['cart']);
+}
+
 ?>
 
 <nav>
     <div class="nav-part nav-left">
         <a href="index.php" class="<?= 'index' == $page ? 'active' : '' ?>">PHP Bases</a>
         <?php if ($connected) : ?>
-            <span style="margin: auto 10px;color: #000000;">Bonjour <?= $_SESSION['user']['firstname'] ?></span>
+            <span style="margin: auto 10px;color: #000000; font-size:larger">Bonjour <?= $_SESSION['user']['firstname'] ?></span>
         <?php endif ?>
     </div>
     <div class=" nav-part nav-right">
-        <a href="cart.php" class="<?= 'cart' == $page ? 'active' : '' ?>">Panier(2)</a>
+        <a href="cart.php" class="<?= 'cart' == $page ? 'active' : '' ?>"><?= $nbPrdCart == 0 ? "Panier" : "Panier (" . $nbPrdCart . ")" ?></a>
         <?php if (!$connected) : ?>
             <a href="login.php" class="<?= 'login' == $page ? 'active' : '' ?>">
             <?php else : ?>
